5bde9edde0aa23474206fb738de083b7a54d7d82
Labasse <labasse@gmail.com>
2024-02-26 20:32:41 +0000

    Enumérer les cas de test (fin)

--- boutique/src/test/java/com/syllab/boutique/metier/PanierTest.java
+++ boutique/src/test/java/com/syllab/boutique/metier/PanierTest.java
@@ -5,6 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PanierTest {
+  // Initialisation
+  // - Usuel
+  // initialisation_panierVide
+  // - Extrême (aucun)
+  // - Erreur (aucun)
+
+  // Ajouter
+  // - Usuel
   @Test
   void ajouter_1Produit() {
     // Arrange
@@ -17,4 +25,20 @@ public class PanierTest {
     // Assertion
     assertEquals(6, panier.getPrixTotal());
   }
+  // ajouter_2ProduitsDifferents
+  // ajouter_1Produit2fois_AdditionneLesQuantites
+  // - Extreme (aucun)
+  // - Erreur
+  // ajouter_quantite0_leveIllegalArgumentException
+  // ajouter_quantiteNegative_leveIllegalArgumentException
+  // ajouter_produitNull_leveNullPointerException
+
+  // Diminuer
+  // - Usuel
+  // diminuer_produitEnQuantite2OuPlus
+  // - Extreme
+  // diminuer_dernierProduitEnQuantite1_panierVide
+  // diminuer_avantDernierProduitEnQuantite1_retireLeProduit
+  // - Erreur
+  // diminuer_produitAbsentDuPanier_leveIllegalArgumentException
 }
