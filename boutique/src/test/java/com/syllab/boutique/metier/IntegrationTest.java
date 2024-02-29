aaaf1527b697d33f8d4d28b3e682e8bd9c095f59
Labasse <labasse@gmail.com>
2024-02-29 21:22:33 +0000

    Effectuer un test d'intégration (début)

---
+++ boutique/src/test/java/com/syllab/boutique/metier/IntegrationTest.java
@@ -0,0 +1,14 @@
+package com.syllab.boutique.metier;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+
+import com.syllab.boutique.NommageRoyOsherove;
+
+@DisplayNameGeneration(NommageRoyOsherove.class)
+public class IntegrationTest {
+  @Test
+  void appliquerReduction_ProduitOfferEtReducSeuilSur2Produits() {
+    
+  }
+}
